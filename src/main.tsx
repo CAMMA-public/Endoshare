18bc63798e8e784ba1ad307afb617c3e87cd8e80
Lorenzo Arboit <LorenzoArboit@wifi-eduroam-interne-172-29-123-233.wifi.unistra.fr>
2025-10-22 23:07:37 +0200

    fixed url docs

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,39 @@
-// src/main.tsx
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, useLocation } from 'react-router-dom';
+import App from './App';
+import './index.css';
+
+/**
+ * Scroll to the element pointed by the URL hash after each navigation.
+ * If no hash is present (or not found), scroll to top.
+ */
+function ScrollToHash() {
+  const { pathname, hash } = useLocation();
+
+  React.useEffect(() => {
+    // try to scroll to an in-page anchor first
+    if (hash) {
+      // decode in case ids contain encoded chars
+      const selector = decodeURIComponent(hash);
+      const el = document.querySelector(selector);
+      if (el) {
+        el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        return;
+      }
+    }
+    // otherwise go to the top on route change
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, [pathname, hash]);
+
+  return null;
+}
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename="/Endoshare">
-    <App />
-  </BrowserRouter>
-)
+  <React.StrictMode>
+    <BrowserRouter basename="/Endoshare">
+      <ScrollToHash />
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
